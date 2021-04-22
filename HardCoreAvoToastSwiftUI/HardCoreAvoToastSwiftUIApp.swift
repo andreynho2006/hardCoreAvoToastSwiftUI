de243e7c4f1da2542f6d55d16a748b2f06aac119
Andrei Cirlan <andreynho2006@gmail.com>
2021-04-22 15:45:24 +0300

    Initial Commit

---
+++ HardCoreAvoToastSwiftUI/HardCoreAvoToastSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HardCoreAvoToastSwiftUIApp.swift
+//  HardCoreAvoToastSwiftUI
+//
+//  Created by Andrei Cirlan on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct HardCoreAvoToastSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
